f99d7b68e0c1716cd6d87b0f9892a9ea070eddbc
Aidan Ohanian <Aidanohanian@gmail.com>
2019-11-08 09:13:26 -0800

    ..

---
+++ UI.portfolio/CalcBiz.h
@@ -0,0 +1,21 @@
+//  CalcC3
+//
+//  Created by Szeto, Colin on 11/1/19.
+//  Copyright © 2019 Szeto, Colin. All rights reserved.
+//
+ 
+#ifndef calc_h
+#define calc_h
+ 
+ 
+#define PLUS 0
+#define MINUS 1
+#define MULTIPLY 2
+#define DIVIDE 3
+#define MODULO 4
+#define SQUARE 5
+ 
+ 
+#include <stdio.h>
+double CalcBiz(double arg1 ,int mathOp,double arg2);
+#endif /* calc_h */
