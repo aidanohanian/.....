4507e99d212be8882382004540be192060d567d3
Aidan Ohanian <Aidanohanian@gmail.com>
2019-11-14 09:32:13 -0800

    saving all work

--- UI.portfolio/viewcontrollers/algocontrollers.m
+++
@@ -1,9 +0,0 @@
-//
-//  algocontrollers.m
-//  UI.portfolio
-//
-//  Created by Ohanian, Aidan on 11/12/19.
-//  Copyright © 2019 Ohanian, Aidan. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
