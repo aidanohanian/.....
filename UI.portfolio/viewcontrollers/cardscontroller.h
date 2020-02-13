536c5f2b4b5971302a28cce631f1a0227e521f78
aidan <aidanohanian@gmail.com>
2020-02-13 09:14:35 -0800

    linked storyboards to main, outlined card storyboard

---
+++ UI.portfolio/viewcontrollers/cardscontroller.h
@@ -0,0 +1,13 @@
+//
+//  cardscontroller.h
+//  UI.portfolio
+//
+//  Created by Ohanian, Aidan on 2/13/20.
+//  Copyright © 2020 Ohanian, Aidan. All rights reserved.
+//
+
+#ifndef cardscontroller_h
+#define cardscontroller_h
+
+
+#endif /* cardscontroller_h */
---
+++ UI.portfolio/viewcontrollers/cardscontroller.m
@@ -0,0 +1,9 @@
+//
+//  cardscontroller.m
+//  UI.portfolio
+//
+//  Created by Ohanian, Aidan on 2/13/20.
+//  Copyright © 2020 Ohanian, Aidan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
